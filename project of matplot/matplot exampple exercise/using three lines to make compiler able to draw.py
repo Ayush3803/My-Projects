c591356230bd1fd3fb0d07aae7e77c545462d5e0
Ayush3803 <122963997+Ayush3803@users.noreply.github.com>
2023-01-20 11:08:42 +0530

    Add files via upload

---
+++ project of matplot/matplot exampple exercise/using three lines to make compiler able to draw.py
@@ -0,0 +1,13 @@
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+ypoints=np.array([3,8,1,10])
+plt.plot(ypoints,marker='.')
+plt.show()
+#using P means plus sign'+' 
+plt.plot(ypoints,marker='P')
+plt.show()
+#using s means plus sign 'square'
+plt.plot(ypoints,marker='s')
+plt.show()
